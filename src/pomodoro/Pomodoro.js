41829b0b08133e1d6eda3282521e48c4562950b0
Alexander Nguyen <41305327+ANguyen214@users.noreply.github.com>
2021-07-29 21:09:32 -0700

    Update Pomodoro.js

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -38,7 +38,7 @@ function Pomodoro() {
   useInterval(() => {
       if (session.timeRemaining === 0) {
         new Audio("https://bigsoundbank.com/UPLOAD/mp3/1482.mp3").play();
-        return setSession(() => nextSession(focusDuration, breakDuration));
+        return setSession(nextSession(focusDuration, breakDuration));
       }
       setSession((event) => nextTick(event));
       calcAria(session);
